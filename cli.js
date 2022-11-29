d3b1be7fc7789f1cfc8c89a0aef4f1099be6f9ab
Ce'Niyah Ellison <69821183+cre24@users.noreply.github.com>
2022-11-29 16:07:40 -0500

    Added in minimist and deleted lines to rework with minimist

--- cli.js
+++ cli.js
@@ -1,58 +1,34 @@
 #!/usr/bin/env node
 
+import minimist from "minimist";
 import moment from "moment-timezone";
 import fetch from "node-fetch";
 
 
-const argv = process.argv.slice(2);
+const arg = minimist(process.argv.slice(2));
 
 // Obtain Timezone
 const timezone = moment.tz.guess()
-if (process.argv.indexOf('-z') > -1) {
-    timezone = argv[process.argv.indexOf('-z') - 1];
-}
 
+//Initialize longitude and latitude
+var longitude;
+var latitude;
 
-if (process.argv.indexOf('-h') > -1) {
-    console.log("Usage: $0 [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE")
-    console.log("  -h\t\tShow this help message and exit.")
-    console.log("  -n, -s\tLatitude: N positive; S negative.")
-    console.log("  -e, -w\tLongitude: E positive; W negative.")
-    console.log("  -z\t\tTime zone: uses /etc/timezone by default.")
-    console.log("  -d 0-6\tDay to retrieve weather: 0 is today; defaults to 1.")
-    console.log("  -j\t\tEcho pretty JSON from open-meteo API and exit.")
-    process.exit(0)
-}
+//Implement timezone and coordinates here
 
-const latitude = 0;
-const longitude = 0;
-
-if (process.argv.indexOf('-n') > -1) {
-	latitude = argv[process.argv.indexOf('-n') - 1]
-}
-
-if (process.argv.indexOf('-s') > -1) {
-	latitude = (argv[process.argv.indexOf('-s') -1]) * (-1);
-}
-
-if (process.argv.indexOf('-w') > -1) {
-	longitude = argv[process.argv.indexOf('-w') - 1];
-}
-if (process.argv.indexOf('-e') > -1) {
-	longitude = (argv[process.argv.indexOf('-e') -1]) * (-1);
-}
-
+// Request to api
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+latitude+'&longitude='+longitude+'&daily=precipitation_hours&timezone='+timezone);
 
 // Data inputed from request
 const data = await response.json();
 
+// Manipulate data from json
 if (process.argv.indexOf('-j') > -1) {
 	console.log(data);
 	process.exit(0);
 }
 
-// Code relating to the day
+// Code relating to the day variable
 const day = 1;
 
 if (process.argv.indexOf('-d') > -1) {
